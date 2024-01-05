fe8936ac7a62ee37393cd9023e032caa113d75cd
Edwin Ade <edwinmbonyjr@gmail.com>
2024-01-05 18:05:09 +0100

    Updated README and __init__

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,7 +5,7 @@ The init file
 creates a blueprint for the app views
 
 '''
-from flask import Flask, Blueprint
+from flask import Blueprint
 
 
 app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')

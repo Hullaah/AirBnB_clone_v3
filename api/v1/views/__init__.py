1a680a6209bc20533c93abdd82a420d26fc77333
Edwin Ade <edwinmbonyjr@gmail.com>
2024-01-05 17:59:26 +0100

    Added url_prefix to app_views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,7 +8,7 @@ creates a blueprint for the app views
 from flask import Flask, Blueprint
 
 
-app_views = Blueprint("app_views", __name__)
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 
 
 from api.v1.views.index import *

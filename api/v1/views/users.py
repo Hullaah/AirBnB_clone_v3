48ab408aae24391692b6d918f7b0eb8fb11c91bb
Edwin Ade <edwinmbonyjr@gmail.com>
2024-01-09 15:49:21 +0100

    Updated module docs

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 '''
+
 This is the implementation of the user routes for
 the AirBnB project API
+
 '''
 from api.v1.views import app_views
 from flask import jsonify, request, abort
